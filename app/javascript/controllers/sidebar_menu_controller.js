4dfbec90227a8264ed953ab48805bf4fa8dfca19
Karine Vieira <kv.vieira1@gmail.com>
2025-02-18 14:27:13 -0300

    fix Glitch in the sidebar during navigation

--- app/javascript/controllers/sidebar_menu_controller.js
+++ app/javascript/controllers/sidebar_menu_controller.js
@@ -3,11 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="sidebar-menu"
 export default class extends Controller {
   connect() {
-    window.addEventListener("turbo:before-visit", () => {
+    window.addEventListener("turbo:before-cache", () => {
       localStorage.setItem("menuScrollPositon", this.element.scrollTop);
     });
 
-    window.addEventListener("turbo:load", () => {
+    window.addEventListener("turbo:before-render", () => {
+      this.element.scrollTop = localStorage.getItem("menuScrollPositon") || 0;
+    });
+    window.addEventListener("turbo:render", () => {
       this.element.scrollTop = localStorage.getItem("menuScrollPositon") || 0;
     });
   }
