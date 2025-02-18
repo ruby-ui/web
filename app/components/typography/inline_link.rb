f75a0c57f84e386b5b9de8f4b656c30ab5149f2c
Karine Vieira <kv.vieira1@gmail.com>
2025-02-18 17:31:59 -0300

    Migrate the typography components to align with Phlex conversions

---
+++ app/components/typography/inline_link.rb
@@ -0,0 +1,14 @@
+module Components
+  module Typography
+    class InlineLink < Components::Base
+      def initialize(href:, **attrs)
+        @href = href
+        @attrs = attrs
+      end
+
+      def view_template(&)
+        a(href: @href, **@attrs, class: "text-primary font-medium hover:underline underline-offset-4", &)
+      end
+    end
+  end
+end
