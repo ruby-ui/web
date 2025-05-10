b1f2bcdcb7f733180e2a13fc8ce43b9a3d0ff9dd
Jean Pierry <pierrybm@gmail.com>
2025-05-10 17:06:08 -0300

    aria-disabled for Switch (#233)
    Co-authored-by: Cirdes <cirdes@linkana.com>

--- app/views/docs/switch.rb
+++ app/views/docs/switch.rb
@@ -27,6 +27,12 @@ class Views::Docs::Switch < Views::Base
         RUBY
       end
 
+      render Docs::VisualCodeExample.new(title: "Aria Disabled", context: self) do
+        <<~RUBY
+          Switch(name: "switch", aria: {disabled: "true"})
+        RUBY
+      end
+
       render Docs::VisualCodeExample.new(title: "With flag include_hidden false", context: self) do
         <<~RUBY
           # Supports the creation of a hidden input to be used in forms inspired by the Ruby on Rails implementation of check_box. Default is true.
