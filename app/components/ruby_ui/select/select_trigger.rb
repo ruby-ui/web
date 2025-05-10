b9ab0132af8a70930510359d6fdb4853f25a28a7
Jean Pierry <pierrybm@gmail.com>
2025-05-10 16:48:12 -0300

    aria-disabled for Select (#232)
    Co-authored-by: Cirdes <cirdes@linkana.com>

--- app/components/ruby_ui/select/select_trigger.rb
+++ app/components/ruby_ui/select/select_trigger.rb
@@ -33,12 +33,12 @@ module RubyUI
 
     def default_attrs
       {
+        type: "button",
+        role: "combobox",
         data: {
           action: "ruby-ui--select#onClick",
           ruby_ui__select_target: "trigger"
         },
-        type: "button",
-        role: "combobox",
         aria: {
           controls: "radix-:r0:",
           expanded: "false",
@@ -46,8 +46,13 @@ module RubyUI
           haspopup: "listbox",
           activedescendant: true
         },
-        class:
-          "truncate w-full flex h-9 items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+        class: [
+          "truncate w-full flex h-9 items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background",
+          "placeholder:text-muted-foreground",
+          "focus:outline-none focus:ring-1 focus:ring-ring",
+          "disabled:cursor-not-allowed disabled:opacity-50",
+          "aria-disabled:cursor-not-allowed aria-disabled:opacity-50 aria-disabled:pointer-events-none"
+        ]
       }
     end
   end
