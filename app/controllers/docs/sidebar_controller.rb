db191472d597231e55ba35c9c97102c9364f9c9a
Lucas Sousa <30642647+lsouoliveira@users.noreply.github.com>
2025-09-09 20:44:37 -0300

    Use the default visual code example for the dialog

--- app/controllers/docs/sidebar_controller.rb
+++ app/controllers/docs/sidebar_controller.rb
@@ -14,10 +14,4 @@ class Docs::SidebarController < ApplicationController
 
     render Views::Docs::Sidebar::InsetExample.new(sidebar_state:)
   end
-
-  def dialog_example
-    sidebar_state = cookies.fetch(:sidebar_state, "true") == "true"
-
-    render Views::Docs::Sidebar::DialogExample.new(sidebar_state:)
-  end
 end
