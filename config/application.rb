3439480a40c2851b1b50d4c272aa439044bc3415
Cirdes <cirdes@linkana.com>
2025-11-20 16:39:06 +0000

    fix tests

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module RubyUiWeb
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.exceptions_app = ->(env) { ErrorsController.action(:not_found).call(env) }
   end
 end
