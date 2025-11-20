e0b03cbf6a0109d4b7e23f91a76497319d192038
Cirdes <cirdes@linkana.com>
2025-11-20 13:40:31 -0300

    Upgrade Rails from 8.0.2.1 to 8.1.1 (#366)
    * Upgrade Rails from 8.0.2.1 to 8.1.1
    
    * fix linter
    
    * fix tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_20_132714) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_20_155307) do
 end
