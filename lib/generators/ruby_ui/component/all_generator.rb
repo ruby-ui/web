8429d24a740f6630193f95131c3942833bb600b9
Seth Horsley <github.dvxhi@sl.isethi.com>
2026-03-09 00:39:52 -0400

    Add gemspec and Rails generators for ruby_ui gem
    - Create ruby_ui.gemspec with gem metadata and dependencies
    - Add lib/ruby_ui/version.rb defining RubyUI::VERSION
    - Implement Rails generators:
      - ruby_ui:component - Generate individual components from app/components/ruby_ui
      - ruby_ui:component:all - Generate all available components
      - ruby_ui:install - Install RubyUI with dependencies
      - ruby_ui:install:docs - Install component documentation
    - Add JavaScript package management utilities (npm, yarn, pnpm, importmap support)
    - Include dependencies.yml for component dependency tracking
    - Add initializer and Tailwind CSS templates for installation

---
+++ lib/generators/ruby_ui/component/all_generator.rb
@@ -0,0 +1,22 @@
+module RubyUI
+  module Generators
+    module Component
+      class AllGenerator < Rails::Generators::Base
+        namespace 'ruby_ui:component:all'
+
+        source_root File.expand_path('../../../../app/components/ruby_ui', __dir__)
+        class_option :force, type: :boolean, default: false
+
+        def generate_components
+          say 'Generating all components...'
+
+          Dir.children(self.class.source_root).each do |folder_name|
+            next if folder_name.ends_with?('.rb')
+
+            run "bin/rails generate ruby_ui:component #{folder_name} --force #{options['force']}"
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/generators/ruby_ui/install/docs_generator.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails/generators'
+
+module RubyUI
+  module Generators
+    module Install
+      class DocsGenerator < Rails::Generators::Base
+        namespace 'ruby_ui:install:docs'
+        source_root File.expand_path('../../../../app/components/ruby_ui', __dir__)
+        class_option :force, type: :boolean, default: false
+
+        def copy_docs_files
+          say 'Installing RubyUI documentation files...'
+
+          docs_file_paths.each do |source_path|
+            dest_filename = File.basename(source_path).sub('_docs', '')
+            copy_file source_path, Rails.root.join('app/views/docs', dest_filename), force: options['force']
+          end
+
+          say ''
+          say 'Documentation installed to app/views/docs/', :green
+        end
+
+        private
+
+        def docs_file_paths
+          Dir.glob(File.join(self.class.source_root, '*', '*_docs.rb'))
+        end
+      end
+    end
+  end
+end
