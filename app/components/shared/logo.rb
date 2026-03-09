cc1263063a9f69245e5aeadb82048e0c8de48848
Seth Horsley <github.dvxhi@sl.isethi.com>
2026-03-09 13:56:05 -0400

    🎷

--- app/components/shared/logo.rb
+++ app/components/shared/logo.rb
@@ -4,12 +4,12 @@ module Components
   module Shared
     class Logo < Components::Base
       def view_template
-        a(href: root_url, class: 'mr-6 flex items-center space-x-2') do
-          Heading(level: 2, class: 'flex items-center pb-0 border-0') do
-            img(src: image_url('logo.svg'), class: 'h-4 block dark:hidden')
-            img(src: image_url('logo_dark.svg'), class: 'h-4 hidden dark:block')
-            span(class: 'sr-only') { 'RubyUI' }
-            Badge(variant: :amber, size: :sm, class: 'ml-2 whitespace-nowrap') { commit_hash }
+        a(href: root_url, class: "mr-6 flex items-center space-x-2") do
+          Heading(level: 2, class: "flex items-center pb-0 border-0") do
+            img(src: image_url("logo.svg"), class: "h-4 block dark:hidden")
+            img(src: image_url("logo_dark.svg"), class: "h-4 hidden dark:block")
+            span(class: "sr-only") { "RubyUI" }
+            Badge(variant: :amber, size: :sm, class: "ml-2 whitespace-nowrap") { commit_hash }
           end
         end
       end
