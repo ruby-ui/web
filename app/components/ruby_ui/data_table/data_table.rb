1daa647013e032cd3952e6f9e9732794c15b0945
Djalma Araujo <djalma@nossomos.cc>
2026-04-24 15:04:44 -0300

    feat(data_table): add DataTable root component
    Renders <turbo-frame> wrapping a <form> with the ruby-ui--data-table
    Stimulus controller. Form supports form-first bulk actions via button
    formaction.

---
+++ app/components/ruby_ui/data_table/data_table.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module RubyUI
+  class DataTable < Base
+    register_element :turbo_frame, tag: "turbo-frame"
+
+    def initialize(id:, action: "", **attrs)
+      @frame_id = id
+      @action = action
+      super(**attrs)
+    end
+
+    def view_template(&block)
+      turbo_frame(id: @frame_id, target: "_top") do
+        div(**attrs) do
+          form(action: @action, method: "post", data: {controller: "ruby-ui--data-table"}) do
+            input(type: "hidden", name: "authenticity_token", value: csrf_token)
+            yield if block
+          end
+        end
+      end
+    end
+
+    private
+
+    def default_attrs
+      {class: "w-full space-y-4"}
+    end
+
+    def csrf_token
+      helpers.form_authenticity_token
+    rescue
+      SecureRandom.hex(32)
+    end
+  end
+end
