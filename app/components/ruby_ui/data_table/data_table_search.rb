72d56f7a5537aa88b9062a07c475a3799ef5adf0
Djalma Araujo <djalma@nossomos.cc>
2026-04-24 15:59:41 -0300

    feat(data_table): wire debounce prop on DataTableSearch
    debounce: 300 (default) | debounce: N (custom) | debounce: false (Enter only).

--- app/components/ruby_ui/data_table/data_table_search.rb
+++ app/components/ruby_ui/data_table/data_table_search.rb
@@ -2,18 +2,19 @@
 
 module RubyUI
   class DataTableSearch < Base
-    def initialize(path:, name: "search", value: nil, frame_id: nil, placeholder: "Search...", **attrs)
+    def initialize(path:, name: "search", value: nil, frame_id: nil, placeholder: "Search...", debounce: 300, **attrs)
       @path = path
       @name = name
       @value = value
       @frame_id = frame_id
       @placeholder = placeholder
+      @debounce = debounce
       super(**attrs)
     end
 
     def view_template
       form_attrs = {method: "get", action: @path}
-      form_attrs[:data] = {turbo_frame: @frame_id} if @frame_id
+      form_attrs[:data] = form_data
 
       form(**attrs.merge(form_attrs)) do
         render RubyUI::Input.new(
@@ -27,6 +28,21 @@ module RubyUI
     end
 
     private
+
+    def debounce_enabled?
+      @debounce && @debounce.to_i > 0
+    end
+
+    def form_data
+      base = {}
+      base[:turbo_frame] = @frame_id if @frame_id
+      if debounce_enabled?
+        base[:controller] = "ruby-ui--data-table-search"
+        base[:"ruby-ui--data-table-search-delay-value"] = @debounce.to_i
+        base[:action] = "input->ruby-ui--data-table-search#submit"
+      end
+      base
+    end
 
     def default_attrs
       {class: "max-w-sm flex-1"}
--- test/components/ruby_ui/data_table/data_table_search_test.rb
+++ test/components/ruby_ui/data_table/data_table_search_test.rb
@@ -19,4 +19,27 @@ class RubyUI::DataTableSearchTest < ActiveSupport::TestCase
     out = RubyUI::DataTableSearch.new(path: "/x", frame_id: "employees").call
     assert_match(/data-turbo-frame="employees"/, out)
   end
+
+  test "emits debounce controller + delay value + action by default" do
+    out = RubyUI::DataTableSearch.new(path: "/x").call
+    assert_match(/data-controller="ruby-ui--data-table-search"/, out)
+    assert_match(/data-ruby-ui--data-table-search-delay-value="300"/, out)
+    assert_match(/data-action="input->ruby-ui--data-table-search#submit"/, out)
+  end
+
+  test "debounce: 500 sets custom delay" do
+    out = RubyUI::DataTableSearch.new(path: "/x", debounce: 500).call
+    assert_match(/data-ruby-ui--data-table-search-delay-value="500"/, out)
+  end
+
+  test "debounce: false disables auto-submit" do
+    out = RubyUI::DataTableSearch.new(path: "/x", debounce: false).call
+    assert_no_match(/data-controller="ruby-ui--data-table-search"/, out)
+    assert_no_match(/data-ruby-ui--data-table-search-delay-value/, out)
+  end
+
+  test "debounce: 0 disables auto-submit" do
+    out = RubyUI::DataTableSearch.new(path: "/x", debounce: 0).call
+    assert_no_match(/data-controller="ruby-ui--data-table-search"/, out)
+  end
 end
